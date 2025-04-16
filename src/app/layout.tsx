625497b18fb5f9d792e5b295bac8b597b16cd3e7
sikakoluchaitanya <sikakoluchaitaya@gmail.com>
2025-04-16 11:43:19 +0530

    added header ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ToastProvider } from "@/components/providers/toast-provider";
 import { AuthProvider } from "@/components/providers/auth-provider";
+import { Header } from "@/components/layout/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
+            <Header/>
             <ToastProvider/>
             {children}
         </AuthProvider>
